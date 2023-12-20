a6492d980599b1144e8a039b93660c435b525bbd
Nikita Kastyshyn <4luckynikita@gmail.com>
2023-12-20 13:36:00 -0800

    05 done

--- problems/05-bind-to-an-arg.js
+++ problems/05-bind-to-an-arg.js
@@ -1,5 +1,7 @@
 function bindToAnArg(func, arg) {
-  // Your code here 
+  return function(...args){
+    return func.call(this, arg, ...args);
+  }
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
